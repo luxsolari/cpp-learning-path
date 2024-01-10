fa497fddacb6b043e16e8c8feae3025756ea63cb
Lux Solari <luxsolari@outlook.com>
2024-01-10 03:06:40 -0300

    Move *nix printing functionality to Suit header, make some parts of code shared by all platforms

--- random-games-archive/Cards/src/Program.cpp
+++ random-games-archive/Cards/src/Program.cpp
@@ -1,42 +1,11 @@
 #include <iostream>
 #include <string>
+#include "enums/Suits.h"
+
 #ifdef _WIN32
 #include "GameWin.h"
-#include "enums/Suits.h"
 #else
-// Define card suits symbols glyphs for *nix
-#define SPADE "♠︎"
-#define CLUB "♣︎"
-#define HEART "♥︎"
-#define DIAMOND "♦︎"
-// Define color codes for *nix
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define BLUE "\033[34m"
-#define WHITE "\033[37m"
-
-// Define card suits enum
-enum SUIT { spade = 1, club, heart, diamond };
-
-void printSuit(int suitToSelect) {
-	switch (suitToSelect) {
-	case spade:
-		std::cout << SPADE;
-		break;
-	case club:
-		std::cout << CLUB;
-		break;
-	case heart:
-		std::cout << HEART;
-		break;
-	case diamond:
-		std::cout << DIAMOND;
-		break;
-	default:
-		std::cout << "Invalid suit." << std::endl;
-		break;
-	}
-}
+#include "GameUnix.h"
 #endif
 int main()
 {
@@ -47,9 +16,12 @@ int main()
 	std::cout << "\n";
 
     // Get instance of GameWin
-    GameWin* gameWin = GameWin::getInstance();
+    //GameWin* gameWin = GameWin::getInstance();
     // Print address of the instance
-    std::cout << "GameWin address: " << gameWin->printAddress() << std::endl;
+    //std::cout << "GameWin address: " << gameWin->printAddress() << std::endl;
+
+    // Get instance of GameUnix
+    GameUnix* gameUnix = GameUnix::getInstance();
 
 	system("pause");
 	return 0;
--- random-games-archive/Cards/src/enums/Suits.h
+++ random-games-archive/Cards/src/enums/Suits.h
@@ -4,15 +4,16 @@
 
 #ifndef CARDS_SUITS_H
 #define CARDS_SUITS_H
+#include <string>
+#include <iostream>
 
 // if WINDOWS
 #ifdef _WIN32
-#include <string>
-#include <iostream>
 #include <io.h>
 #include <Windows.h>
 #include <minwindef.h>
 #include <fcntl.h>
+#endif
 
 enum class Suits {
     HEARTS = 1,
@@ -22,10 +23,13 @@ enum class Suits {
 };
 
 enum class Colors {
+    BLUE = 1,
+    GREEN = 2,
     RED = 4,
     BLACK = 7
 };
 
+#ifdef _WIN32
 // color setter
 inline void SetColor(WORD color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
@@ -53,8 +57,8 @@ inline std::wostream& operator<<(std::wostream& os, const color& c) {
 // instances of helper class to avoid need to remember 4 is red, etc
 static const color red((WORD) Colors::RED);
 static const color black((WORD) Colors::BLACK);
-static const color blue(1);
-static const color green(2);
+static const color blue((WORD) Colors::BLUE);
+static const color green((WORD) Colors::GREEN);
 
 // suit symbol for Windows
 const std::wstring HEARTS_SYMBOL     = L"\u2665";
@@ -83,6 +87,41 @@ namespace SuitUtils {
                 break;
         }
         _setmode(_fileno(stdout), _O_TEXT);
+    }
+}
+// if UNIX
+#else
+// Define card suits symbols glyphs for *nix
+const std::string SPADE = "♠︎";
+const std::string CLUB = "♣︎";
+const std::string HEART = "♥︎";
+const std::string DIAMOND = "♦︎";
+
+// Define color codes for *nix
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string BLUE = "\033[34m";
+const std::string WHITE = "\033[37m";
+
+namespace SuitUtils {
+    void printSuit(int suitToSelect) {
+        switch (suitToSelect) {
+            case static_cast<int>(Suits::HEARTS):
+                std::cout << RED << HEART << WHITE;
+                break;
+            case static_cast<int>(Suits::DIAMONDS):
+                std::cout << RED << DIAMOND << WHITE;
+                break;
+            case static_cast<int>(Suits::CLUBS):
+                std::cout << CLUB;
+                break;
+            case static_cast<int>(Suits::SPADES):
+                std::cout << SPADE;
+                break;
+            default:
+                std::cout << "Invalid suit." << std::endl;
+                break;
+        }
     }
 }
 
