994f0a6b8d9b86ca6de1abec97f77180868247d1
Lux Solari <luxsolari@outlook.com>
2024-01-14 22:56:13 -0300

    Finally get proper utf-8 output on ncurses with CLion IDE.

--- random-games-archive/Cards/src/Program.cpp
+++ random-games-archive/Cards/src/Program.cpp
@@ -1,14 +1,38 @@
-#include "utils/ConsoleFactory.h"
+#include <clocale>
+#include <iostream>
+#include <ncurses.h>
 
 int main() {
-    auto console = ConsoleFactory::createConsole();
-    console->printToConsole("Hello World!\n");
-    console->printToConsoleWide(L"\u2665");
-    console->printToConsoleAtLocation(10, 10, "Hello World!\n");
-    console->waitEnterKey();
-    console->drawSquareBorder(10, 10, {10, 10});
-    console->drawSquareBorder(10, 10, {20, 10});
-    console->waitEnterKey();
+    // get terminal type
+    char *termType = getenv("TERM");
+    std::cout << "Terminal type: " << termType << std::endl;
+    // set locale
+    setlocale(LC_ALL, "C/UTF-8/C/C/C/C");
+
+    initscr(); // Initialize ncurses
+    // draw border around screen
+    box(stdscr, 0, 0);
+
+    // set invisible cursor
+    curs_set(0);
+
+    // get screen size
+    int x{0};
+    int y{0};
+    getmaxyx(stdscr, y, x);
+    std::string message = "Hello, world!";
+
+    // print on center of screen
+    mvprintw(y / 2, (x / 2) - (message.length() / 2), "Hello, world!");
+    mvprintw(y / 2, (x / 2) - (message.length() / 2), "  ♠︎ ♣︎ ♥︎ ♦︎  ");
+
+    // refresh screen
+    refresh();
+    // wait for enter key
+    std::wcin.get();
+
+    // end ncurses
+    endwin();
 
     return 0;
 }
