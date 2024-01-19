86a641ad0eaf866fd4a285aff4c95c44cf9a050c
Lux Solari <luxsolari@outlook.com>
2024-01-19 17:33:35 -0300

    add unix input class

---
+++ games-archive/Cards/src/utils/io/impl/UnixInput.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Lux Solari on 19/01/2024.
+//
+#ifdef UNIX_PLATFORM
+#include "UnixInput.h"
+
+#endif //UNIX_PLATFORM
+
+void UnixInput::Update() {
+
+}
+
+bool UnixInput::IsKeyDown(int key) {
+    return false;
+}
+
+bool UnixInput::IsKeyPressed(int key) {
+    return false;
+}
+
+bool UnixInput::IsKeyReleased(int key) {
+    return false;
+}
